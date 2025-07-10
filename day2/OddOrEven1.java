0270456e213eb38de326eec315d34ce5bca3a6f1
Yuvarani P <104048459+YuvaraniPalani@users.noreply.github.com>
2025-07-10 19:19:01 +0530

    Add files via upload

---
+++ day2/OddOrEven1.java
@@ -0,0 +1,20 @@
+package Week1.day2;
+
+public class OddOrEven1 {
+
+	public static void main(String[] args) {
+
+		int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		for (int i = 0; i < values.length; i++) {
+
+			// if(values[i]% 2 != 0)
+			if (values[i] % 2 == 1)
+
+				System.out.println(values[i] + " is an odd number");
+
+			else
+				System.out.println(values[i] + " is an even number");
+		}
+	}
+
+}
