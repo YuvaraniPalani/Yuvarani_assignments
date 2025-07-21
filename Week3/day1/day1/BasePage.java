4426c88f757d943a14b88090d0820bca687f03a6
Yuvarani P <104048459+YuvaraniPalani@users.noreply.github.com>
2025-07-21 18:18:27 +0530

    Add files via upload

---
+++ Week3/day1/day1/BasePage.java
@@ -0,0 +1,20 @@
+package Week3.day1;
+
+public class BasePage {
+//parent class for method overriding
+	public static void main(String[] args) {
+		
+	}
+	public void findElement() {
+		System.out.println("findelement from basepage");
+	}
+	public void clickElement() {
+		System.out.println("clickElement from basepage");
+	}
+	public void enterText() {
+		System.out.println("enterText from basepage");
+	}
+	public void performCommonTasks() {
+		System.out.println("performCommonTasks from basepage");
+	}
+}
