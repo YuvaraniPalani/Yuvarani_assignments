fbeb7dab6d921eaeeca4dd95b530b88ea3e443da
Yuvarani P <104048459+YuvaraniPalani@users.noreply.github.com>
2025-08-13 14:41:01 +0530

    Add files via upload

---
+++ CucumberAssignment/java/steps/BaseClassfb.java
@@ -0,0 +1,13 @@
+package steps;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseClassfb {
+	
+	public static ChromeOptions option;   
+	public static ChromeDriver driver;
+
+	}
+
+
